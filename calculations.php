3f7603f79705959dda8b89a181481063c0200daf
Erik Veenstra <zerikveenstraz@gmail.com>
2021-12-29 21:59:38 +0100

    Removed useless file after rename, filled calculations.php

---
+++ calculations.php
@@ -0,0 +1,16 @@
+<?php  
+class Calculations 
+{
+	/**
+	 * Adds the given integers to eachother and returns the total
+	 * 
+	 * @param int $a
+	 * @param int $b
+	 * @return int $total
+	 */
+	public function add(int $a, int $b) 
+	{
+		return $a + $b;
+	}
+}
+?>
